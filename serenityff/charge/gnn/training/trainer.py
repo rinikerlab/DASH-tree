cb706251578d36c993bcb4e671dc0721b921b937
Niels Maeder <maedern@student.ethz.ch>
2022-07-25 14:10:48 +0200

    trainer skeleton added

--- serenityff/charge/gnn/training/trainer.py
+++ serenityff/charge/gnn/training/trainer.py
@@ -1,4 +1,6 @@
+from typing import Optional, Sequence
 import torch
+from serenityff.charge.gnn.utils import CustomData
 
 
 class Trainer:
@@ -8,3 +10,38 @@ class Trainer:
     @property
     def model(self) -> torch.nn.Module:
         return self._model
+
+    @property
+    def optimizer(self) -> torch.optim.Optimizer:
+        return self._optimizer
+
+    @property
+    def data(self) -> Sequence[CustomData]:
+        return self._data
+
+    @property
+    def train_data(self) -> Sequence[CustomData]:
+        return self._train_data
+
+    @property
+    def test_data(self) -> Sequence[CustomData]:
+        return self._test_data
+
+    @property
+    def eval_data(self) -> Sequence[CustomData]:
+        return self._eval_data
+
+    def train_model(
+        self,
+        epochs,
+        batchsize: Optional[int] = 64,
+        device: Optional[torch.device] = torch.device("cpu"),
+    ):
+        assert self.train_data
+        assert self.model
+        assert self.optimizer
+
+        self.model.train()
+
+        for _ in range(epochs):
+            pass

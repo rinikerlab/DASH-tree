07077a73ca8839fee181d7c8b1dad12427edc2a6
mlehner <marc.lehner@phys.chem.ethz.ch>
2022-08-05 13:47:41 +0200

    added docu to node

--- serenityff/charge/tree/node.py
+++ serenityff/charge/tree/node.py
@@ -1,9 +1,6 @@
-from typing import List
-import os
+from typing import Dict, List
 import numpy as np
 import pandas as pd
-
-from concurrent.futures import ThreadPoolExecutor
 
 from serenityff.charge.tree.atom_features import atom_features
 
@@ -45,6 +42,14 @@ class node:
         return [hash(str(self.level) + str(atom)) for atom in self.atoms]
 
     def add_node(self, node):
+        """
+        Add a node to the current node (merge)
+
+        Parameters
+        ----------
+        node : node
+            The node to add to the current node
+        """
         self.atoms.extend(node.atoms)
         self._update_statistics(node)
         self.hashes.extend(node.hashes)
@@ -53,6 +58,14 @@ class node:
             self.add_child(child)
 
     def _update_statistics(self, other):
+        """
+        helper function to merge two nodes. Updates the statistics of the current node with the statistics of the other node
+
+        Parameters
+        ----------
+        other : node
+            The node to merge with the current node
+        """
         # merge statistics
         # combine result weighted by counts
         self.result = (self.result * self.count + other.result * other.count) / (self.count + other.count)
@@ -73,6 +86,14 @@ class node:
         self.count += other.count
 
     def add_child(self, child):
+        """
+        Add a child to the current node
+
+        Parameters
+        ----------
+        child : node
+            The child to add to the current node
+        """
         if child in self.children:
             idx = self.children.index(child)
             self.children[idx]._update_statistics(child)
@@ -82,6 +103,15 @@ class node:
             self.children.append(child)
 
     def prune(self, threshold=0.001):
+        """
+        Prune the tree to remove nodes with a stdDeviation below a threshold.
+        (recursive function)
+
+        Parameters
+        ----------
+        threshold : float, optional
+            pruning threshold, by default 0.001
+        """
         if hasattr(self, "stdDeviation") and self.stdDeviation != np.nan:
             adjusted_threshold = threshold * ((self.level / 8) + (np.exp(self.level - 20)))
 
@@ -93,14 +123,22 @@ class node:
         for child in self.children:
             child.prune(threshold)
 
-    def to_file(self, file):
-        node_dict_list = []
-        node_dict_list, number = self.node_to_dict(node_dict_list, number=0)
-        df = pd.DataFrame(node_dict_list)
-        df.to_csv(file)
-        print(f"Saved to {file} with {number} nodes")
+    def to_file(self, file_path: str):
+        """
+        Write the tree to a file.
 
-    def node_to_dict(self, node_dict, number, parent_number=0):
+        Parameters
+        ----------
+        file : str
+            The file to write to
+        """
+        node_dict_list = []
+        node_dict_list, number = self._node_to_dict(node_dict_list, number=0)
+        df = pd.DataFrame(node_dict_list)
+        df.to_csv(file_path)
+        print(f"Saved to {file_path} with {number} nodes")
+
+    def _node_to_dict(self, node_dict: Dict, number: int, parent_number: int = 0):
         this_node = {
             "level": self.level,
             "atom": self.atoms,
@@ -119,10 +157,18 @@ class node:
             node_dict, number = child.node_to_dict(node_dict, number, parent_number)
         return (node_dict, number)
 
-    def from_file(self, file):
+    def from_file(self, file_path: str):
+        """
+        Read a tree from a file.
+
+        Parameters
+        ----------
+        file : str
+            The file to read from (needs to be csv)
+        """
         try:
             df = pd.read_csv(
-                file,
+                file_path,
                 index_col=0,
                 dtype={
                     "level": np.int32,
@@ -135,17 +181,11 @@ class node:
                     "number": np.int32,
                 },
             )
-            self.node_from_df(df, index=0)
+            self._node_from_df(df, index=0)
         except FileNotFoundError:
-            Warning(f"File {file} not found")
+            Warning(f"File {file_path} not found")
 
-    def from_files(self, file_folder, num_workers=1):
-        num_files = len(os.listdir(file_folder))
-        with ThreadPoolExecutor(max_workers=num_workers) as executor:
-            results = executor.map(self.from_file, [file_folder + f"/{i}.csv" for i in range(num_files)])
-        self.children = list(results)
-
-    def node_from_df(self, df, index, parent_level=-1):
+    def _node_from_df(self, df: pd.DataFrame, index: int, parent_level: int = -1):
         if index >= len(df):
             return index
         this_node = df.iloc[index]
@@ -167,7 +207,7 @@ class node:
                 while children_found < num_children:
                     index += 1
                     child = self.__class__()
-                    temp_index = child.node_from_df(df, index, parent_level=self.level)
+                    temp_index = child._node_from_df(df, index, parent_level=self.level)
                     if temp_index is None:  # handling of safty for children
                         children_found = num_children
                     else:
@@ -178,7 +218,7 @@ class node:
                 while children_found < num_children:
                     index += 1
                     child = self.__class__()
-                    temp_index = child.node_from_df(df, index, parent_level=self.level)
+                    temp_index = child._node_from_df(df, index, parent_level=self.level)
                     if temp_index is None:  # handling of safty for children
                         children_found = num_children
                     else:
@@ -187,7 +227,7 @@ class node:
                         index = temp_index
         return index
 
-    def get_attribute_from_df_line(self, df_line):
+    def _get_attribute_from_df_line(self, df_line):
         self.level = df_line["level"]
         atom_data = df_line["atom"].split(" ") if isinstance(df_line["atom"], str) else []
         self.atom = atom_features(data=atom_data)
@@ -201,17 +241,42 @@ class node:
         except KeyError:
             pass
 
-    def node_is_similar(self, other, min_std_deviation=0.001):
+    def node_is_similar(self, other, min_deviation: float = 0.001):
+        """
+        Check if two nodes are similar (if results are close enough)
+
+        Parameters
+        ----------
+        other : node
+            The other node to compare to
+        min_deviation : float, optional
+            threhold for similarity, by default 0.001
+
+        Returns
+        -------
+        bool
+            True if similar, False otherwise
+        """
         if self.level in [0, 1, 2, 3]:
             return False
-        if abs(self.result - other.result) < min_std_deviation:
+        if abs(self.result - other.result) < min_deviation:
             return True
         return False
 
-    def try_to_merge_similar_branches(self, min_std_deviation=0.001, children_overlap_acceptance=0.6):
+    def try_to_merge_similar_branches(self, min_deviation=0.0001, children_overlap_acceptance=0.8):
+        """
+        Try to merge similar branches.
+
+        Parameters
+        ----------
+        min_deviation : float, optional
+            threhold for similarity, by default 0.001
+        children_overlap_acceptance : float, optional
+            threshold for overlap similarity, by default 0.6
+        """
         for idx, child in enumerate(self.children):
             for other_child in self.children[idx + 1 :]:
-                if child.node_is_similar(other_child, min_std_deviation=min_std_deviation):
+                if child.node_is_similar(other_child, min_std_deviation=min_deviation):
                     control_bool = True
                     child_match = 0
                     for node_i in child.children:
@@ -219,7 +284,7 @@ class node:
                             child_match += 1
                             if not node_i.node_is_similar(
                                 other_child.children[other_child.children.index(node_i)],
-                                min_std_deviation=min_std_deviation,
+                                min_std_deviation=min_deviation,
                             ):
                                 control_bool = False
                                 break
@@ -230,10 +295,22 @@ class node:
                     ):
                         child.add_node(other_child)
                         self.children.remove(other_child)
-                        return True
-            child.try_to_merge_similar_branches(min_std_deviation=min_std_deviation)
+            child.try_to_merge_similar_branches(min_std_deviation=min_deviation)
 
-    def get_tree_length(self, length_dict):
+    def get_tree_length(self, length_dict: Dict):
+        """
+        Get the length of the tree. And store it in a dictionary with level as key.
+
+        Parameters
+        ----------
+        length_dict : Dict
+            Dictionary to store the length of the tree
+
+        Returns
+        -------
+        Dict
+            Dictionary with level as key and length as value
+        """
         length_dict[self.level] += 1
         for child in self.children:
             length_dict = child.get_tree_length(length_dict)

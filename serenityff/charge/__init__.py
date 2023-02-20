0abf8b8a6283ee3adcf8c5dd1f00e64168de5b74
mlehner <marc.lehner@phys.chem.ethz.ch>
2023-02-20 14:12:39 +0100

    removed top level import forwarding

--- serenityff/charge/__init__.py
+++ serenityff/charge/__init__.py
@@ -1,12 +1,12 @@
-from serenityff.charge.gnn import ChargeCorrectedNodeWiseAttentiveFP, NodeWiseAttentiveFP, Extractor, Trainer
+# from serenityff.charge.gnn import ChargeCorrectedNodeWiseAttentiveFP, NodeWiseAttentiveFP, Extractor, Trainer
 
 from . import _version
 
 __version__ = _version.get_versions()["version"]
 
-__all__ = [
-    Trainer,
-    Extractor,
-    ChargeCorrectedNodeWiseAttentiveFP,
-    NodeWiseAttentiveFP,
-]
+# __all__ = [
+#    Trainer,
+#    Extractor,
+#    ChargeCorrectedNodeWiseAttentiveFP,
+#    NodeWiseAttentiveFP,
+# ]

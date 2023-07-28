8017694462f773c215279b0b0233ff1081a5c787
mlehner <marc.lehner@phys.chem.ethz.ch>
2023-07-28 12:52:58 +0200

    weight loss 0 less

--- serenityff/charge/gnn/training/trainer.py
+++ serenityff/charge/gnn/training/trainer.py
@@ -537,9 +537,12 @@ class Trainer:
 
 def cross_entropy_loss_for_torsionProfile(x, y, num_buckets=100, device="cpu"):
     y_bucket = torch.bucketize(y.unsqueeze(1), torch.tensor(np.linspace(-0.5, 0.5, num_buckets, True), device=device))
+    y_weight = torch.ones(y_bucket.shape, device=device)
+    y_weight[45:55] = 0.1
+    y_bucket *= y_weight
     bin_tensor = torch.zeros(x.shape, device=device)
     bin_tensor.scatter_(1, y_bucket, 1)
-    # loss_fn = torch.nn.MSELoss()
-    loss_fn = torch.nn.CrossEntropyLoss()
+    loss_fn = torch.nn.MSELoss()
+    # loss_fn = torch.nn.CrossEntropyLoss()
     loss = loss_fn(x, bin_tensor)
     return loss

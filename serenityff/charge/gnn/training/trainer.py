d8146b4f5db78ca07db6580a70d97db5c68154b7
mlehner <marc.lehner@phys.chem.ethz.ch>
2023-08-14 08:20:51 +0200

    incease 0 peak penalty

--- serenityff/charge/gnn/training/trainer.py
+++ serenityff/charge/gnn/training/trainer.py
@@ -538,7 +538,7 @@ class Trainer:
 def cross_entropy_loss_for_torsionProfile(x, y, num_buckets=100, device="cpu"):
     y_bucket = torch.bucketize(y.unsqueeze(1), torch.tensor(np.linspace(-0.5, 0.5, num_buckets, True), device=device))
     y_weight = torch.ones(y_bucket.shape, device=device, dtype=torch.float32)
-    y_weight[45:55] = 0.001
+    y_weight[45:55] = 0.0001
     bin_tensor = torch.zeros(x.shape, device=device)
     bin_tensor.scatter_(1, y_bucket, 1)
     loss_fn = torch.nn.MSELoss()

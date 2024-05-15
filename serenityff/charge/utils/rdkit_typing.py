976de3a4b679168c40d85747a30444859d0cd90f
Marc Thierry Lehner <35803803+MTLehner@users.noreply.github.com>
2024-05-15 20:44:30 +0200

    Update rdkit_typing.py

--- serenityff/charge/utils/rdkit_typing.py
+++ serenityff/charge/utils/rdkit_typing.py
@@ -1,7 +1,6 @@
 # typing tools for the use of rdkit
 from rdkit import Chem
-import typing
 
-Molecule: typing.TypeAlias = Chem.rdchem.Mol
-Atom: typing.TypeAlias = Chem.rdchem.Atom
-Bond: typing.TypeAlias = Chem.rdchem.Bond
+Molecule = Chem.rdchem.Mol
+Atom = Chem.rdchem.Atom
+Bond = Chem.rdchem.Bond

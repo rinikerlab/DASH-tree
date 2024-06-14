3a2cf04f8e73d9648eda6102b9e9508c410f76a9
Niels Maeder <maedern@student.ethz.ch>
2024-06-14 15:48:20 +0200

    add functionality and tests

--- serenityff/charge/tests/test_retrieve_data.py
+++ serenityff/charge/tests/test_retrieve_data.py
@@ -1,7 +1,59 @@
-from serenityff.charge.tree.retrieve_data import download_tree_data_from_archive, ADDITIONAL_DATA_DIR
+from pathlib import Path
+
+from serenityff.charge.tree.retrieve_data import (
+    data_is_complete,
+    download_tree_data_from_archive,
+    ADDITIONAL_DATA_DIR,
+    extract_data,
+    get_additional_data,
+)
 import pytest
+from serenityff.charge.utils.exceptions import DataDownloadError, DataExtractionError
+from serenityff.charge.data import default_dash_tree_path
+
+TEST_ZIP = Path(__file__).parent / "testfiles" / "test_archive.zip"
 
 
 def test_paths() -> None:
     assert "/serenityff/charge/data" in ADDITIONAL_DATA_DIR.as_posix()
     assert (ADDITIONAL_DATA_DIR / ".gitkeep").exists()
+
+
+@pytest.mark.parametrize("url", [("invalidurl"), (None)])
+def test_download_tree_data_from_archive_fails(url) -> None:
+    with pytest.raises(DataDownloadError):
+        download_tree_data_from_archive(url=url)
+
+
+@pytest.mark.parametrize("zip_archive", [("notexisting"), (123), (None)])
+def test_extract_data_fails(zip_archive) -> None:
+    with pytest.raises(DataExtractionError):
+        extract_data(zip_archive=zip_archive)
+
+
+def test_extract_data(tmp_path: Path) -> None:
+    test_folder = tmp_path / "extracted"
+    test_folder.mkdir()
+    extract_data(zip_archive=TEST_ZIP, folder=test_folder)
+    assert data_is_complete(folder=test_folder)
+
+
+@pytest.mark.parametrize("folder, exception", [("faulty", None), (1, TypeError), (None, TypeError)])
+def test_data_is_complete_fails(folder, exception):
+    if exception is None:
+        assert not data_is_complete(folder)
+        return
+    with pytest.raises(exception):
+        data_is_complete(folder)
+
+
+def test_data_is_complete(tmp_path: Path):
+    test_folder = tmp_path / "extracted"
+    test_folder.mkdir()
+    extract_data(zip_archive=TEST_ZIP, folder=test_folder)
+    assert data_is_complete(folder=test_folder)
+    assert data_is_complete(folder=default_dash_tree_path)
+
+
+def test_get_additional_data_already_existing():
+    assert get_additional_data(folder=default_dash_tree_path)
--- serenityff/charge/tree/retrieve_data.py
+++ serenityff/charge/tree/retrieve_data.py
@@ -1,17 +1,17 @@
 """Functionality to obtain the DASH properties data from ETH research archive."""
 from urllib.request import urlretrieve
 from pathlib import Path
+import zipfile
 
 from serenityff.charge.data import default_dash_tree_path
-from serenityff.charge.utils.exceptions import DataIncompleteError
+from serenityff.charge.utils.exceptions import DataDownloadError, DataExtractionError
 
 ADDITIONAL_DATA_DIR = Path(__file__).parent.parent / "data" / "additional_data"
 ZIP_FILE = ADDITIONAL_DATA_DIR / "dash_data_download.zip"
-
 DATA_URL = ""
 
 
-def download_tree_data_from_archive(url: str = DATA_URL) -> None:
+def download_tree_data_from_archive(url: str = DATA_URL, file: Path = ZIP_FILE) -> None:
     """Download additional DASH Properties data.
 
     Gets the data uploaded to the ETH-research archive for the DASH-Props
@@ -19,18 +19,33 @@ def download_tree_data_from_archive(url: str = DATA_URL) -> None:
 
     Args:
         url (str, optional): Where to get the data. Defaults to "".
+        file (Path, optional): Zip archive to write data to. Defaults to "".
     """
-    ...
+    try:
+        print("Downloading data from ETH research archive...")
+        urlretrieve(url=url, filename=file)
+    except ValueError:
+        raise DataDownloadError("The provided Url for the extra data doesn't exist.")
+    except TypeError:
+        raise DataDownloadError("URL for additional data cannot be None")
 
 
-def extract_data(zip_archive: Path = ZIP_FILE) -> None:
+def extract_data(zip_archive: Path = ZIP_FILE, folder: Path = ADDITIONAL_DATA_DIR) -> None:
     """
     Extract the Downloaded Zip archive to be readable by the DASH-Tree constructor.
 
     Args:
         zip_archive (Path, optional): Zip archive to extract. Defaults to ZIP_FILE.
+        folder (Path, optional): Where to extract data to. Defaults to ADDITIONAL_DATA_DIR.
     """
-    ...
+    try:
+        with zipfile.ZipFile(zip_archive) as zip_ref:
+            print("Extracting data...")
+            zip_ref.extractall(folder)
+    except (FileNotFoundError, AttributeError):
+        raise DataExtractionError("Zip to archive was not found.")
+    except KeyboardInterrupt:
+        pass
 
 
 def data_is_complete(folder: Path = ADDITIONAL_DATA_DIR) -> bool:
@@ -47,15 +62,17 @@ def data_is_complete(folder: Path = ADDITIONAL_DATA_DIR) -> bool:
     Returns:
         bool: True if all files listed in the 'filelist.txt' are present.
     """
-    for suffix in (".gz", ".h5"):
+    if not isinstance(folder, Path):
+        folder = Path(folder)
+    for suffix in ("*.gz", "*.h5"):
         default_files = set(file.name for file in default_dash_tree_path.glob(suffix))
-        loaded_files = set(file.name for file in ADDITIONAL_DATA_DIR.glob(suffix))
+        loaded_files = set(file.name for file in folder.glob(suffix))
         if len(default_files.intersection(loaded_files)) != len(default_files):
             return False
     return True
 
 
-def get_additional_data(url: str = DATA_URL, zip_archive: Path = ZIP_FILE, folder: Path = ADDITIONAL_DATA_DIR) -> None:
+def get_additional_data(url: str = DATA_URL, zip_archive: Path = ZIP_FILE, folder: Path = ADDITIONAL_DATA_DIR) -> bool:
     """Download and extract additional data from ETH research archive.
 
     Args:
@@ -63,12 +80,15 @@ def get_additional_data(url: str = DATA_URL, zip_archive: Path = ZIP_FILE, folde
         zip_archive (Path, optional): where to store download. Defaults to ZIP_FILE.
         folder (Path, optional): where to store additional data.
 
+    Returns:
+        bool: True if files already exist or were downloaded and extracted succesfully.
+
     Raises:
         DataNotComplete: Throw when not all data files necessary where found.
     """
+    if data_is_complete(folder=folder):
+        print("Data is already loaded.")
+        return True
     download_tree_data_from_archive(url=url)
     extract_data(zip_archive=zip_archive)
-    if not data_is_complete(folder=folder):
-        raise DataIncompleteError(
-            "Not all files necessary for DASH-Props were found. " "Please download and extract them again"
-        )
+    return data_is_complete(folder=folder)

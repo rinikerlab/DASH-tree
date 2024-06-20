60ab6e668ff3add95f9164128c38c45a11d7fc4e
Niels Maeder <maedern@student.ethz.ch>
2024-06-20 10:13:02 +0200

    fist test

--- tests/serenityff/charge/tree/test_tree_factory.py
+++ tests/serenityff/charge/tree/test_tree_factory.py
@@ -1,10 +1,7 @@
-import pytest
-
 from serenityff.charge.tree.tree_factory import Forest
 
 
-# @pytest.mark.slow
-# def test_MBIS_DASH_tree() -> None:
-#     tree = Forest.get_MBIS_DASH_tree()
-#     tree.default_std_column = "std"
-#     tree.default_value_column = "result"
+def test_MBIS_DASH_tree() -> None:
+    tree = Forest.get_MBIS_DASH_tree(preload=False)
+    tree.default_std_column = "std"
+    tree.default_value_column = "result"

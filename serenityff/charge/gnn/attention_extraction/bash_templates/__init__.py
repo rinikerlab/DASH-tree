7b8f514e92ff0f188b3125c268aa7370e8e2e393
Niels Maeder <maedern@student.ethz.ch>
2025-12-11 13:41:45 +0100

    fix F401

--- serenityff/charge/gnn/attention_extraction/bash_templates/__init__.py
+++ serenityff/charge/gnn/attention_extraction/bash_templates/__init__.py
@@ -2,3 +2,9 @@
 
 from .cleaner import CLEANER_CONTENT
 from .worker import get_lsf_worker_content, get_slurm_worker_content
+
+__all__ = [
+    "get_lsf_worker_content",
+    "get_slurm_worker_content",
+    "CLEANER_CONTENT",
+]
